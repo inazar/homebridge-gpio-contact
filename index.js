124f6aeed38f0392753da52e2870c6fb533a3fbe
Morten Vejhe <morten@Mortens-MacBook-Pro.local>
2018-02-26 21:06:02 +0100

    Made GPIOSwitch a class

--- index.js
+++ index.js
@@ -9,75 +9,79 @@ module.exports = function(homebridge)
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  // Register this accessory as 'GPIO-OnOff'
-  homebridge.registerAccessory("homebridge-gpio-switch", "homebridge-gpio-switch", GPIOSwitch);
+  homebridge.registerAccessory(
+      "homebridge-gpio-switch", 
+      "GPIO-Switch", 
+      function(log, config){ return new GPIOSwitch(homebridge, log, config);});
 };
 
-function GPIOSwitch(log, config)
-{
-  this.log = log;
 
-  this.config = Object.assign(
-    {},
+class GPIOSwitch {
+
+    constructor()
+    {    
+        this.log = log;
+        
+        this.config = Object.assign(
+        {},
+        {
+            switch: Object.assign(
+                {},
+                {
+                    activeValue: 1,
+                },
+                config.switch),
+            rpioSettings: Object.assign(
+                {},
+                {
+                    gpiomem: true,
+                    mapping: "physical"
+                },
+                config.rpioSettings),
+            initialFallbackState: false,
+            type: Object.assign(
+                {},
+                {
+                    manufacturer: "Opensource Community",
+                    model: "GPIO Switch",
+                    serialNumber: "Version 2.0.0"
+                },
+                config.type
+            )
+        }, config);
+        
+        if (process.geteuid() !== 0 && this.config.rpioSettings.gpiomem === false)
+        {
+            log("WARN! WARN! WARN! Using /dev/mem and not running as root");
+        }
+        rpio.init(this.config.rpioSettings);
+        
+        this.service = new Service.Switch(this.config.name, this.config.name);
+        
+        this.infoService = new Service.AccessoryInformation();
+        this.infoService
+            .setCharacteristic(Characteristic.Manufacturer, this.config.type.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.config.type.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.config.type.serialNumber);
+        
+        // Initialize the switch service
+        
+        this._switch = new Switch(this.log, this.config.switch.pin, this.config.switch.activeValue);
+        
+        this.service
+            .getCharacteristic(Characteristic.On)
+            .on("set", this.setState.bind(this));
+    }
+
+    getServices()
     {
-        switch: Object.assign(
-            {},
-            {
-                activeValue: 1,
-            },
-            config.switch),
-        rpioSettings: Object.assign(
-            {},
-            {
-                gpiomem: true,
-                mapping: "physical"
-            },
-            config.rpioSettings),
-        initialFallbackState: Characteristic.On.Off,
-        type: Object.assign(
-            {},
-            {
-                manufacturer: "Opensource Community",
-                model: "RaspPi GPIO Switch",
-                serialNumber: "Version 2.0.0"
-            },
-            config.type
-        )
-    }, config);
+        return [this.service];
+    }
 
-  if (process.geteuid() !== 0 && this.config.rpioSettings.gpiomem === false)
-  {
-      log("WARN! WARN! WARN! Using /dev/mem and not running as root");
-  }
-  rpio.init(this.config.rpioSettings);
-  
-  this.service = new Service.Switch(this.config.name, this.config.name);
-
-  this.infoService = new Service.AccessoryInformation();
-  this.infoService
-      .setCharacteristic(Characteristic.Manufacturer, this.config.type.manufacturer)
-      .setCharacteristic(Characteristic.Model, this.config.type.model)
-      .setCharacteristic(Characteristic.SerialNumber, this.config.type.serialNumber);
-
-  // Initialize the switch service
-
-  this._switch = new Switch(this.log, this.config.switch.pin, this.config.switch.activeValue);
-  
-  this.service
-      .getCharacteristic(Characteristic.On)
-      .on("set", this.setState.bind(this));
-}
-
-GPIOSwitch.prototype = {
-  getServices: function()
-  {
-    return [this.service];
-  },
-
-  setState: function(value, callback)
-  {
-    this.log("New value: " + value);
-    this._switch.state = value;
-    callback();
-  }
+    setState(value, callback)
+    {
+        this.log("New value: " + value);
+        this._switch.state = value;
+        callback();
+    }
 }
